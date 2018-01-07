1736c748a900b5b58d23fbe9e3c1cc35248f33c6
jeanWin <501821782@qq.com>
2018-01-07 09:35:35 +0800

    aa

--- core/mybatis-generator-core/src/test/java/org/mybatis/generator/MyTest/MgbTest.java
+++ core/mybatis-generator-core/src/test/java/org/mybatis/generator/MyTest/MgbTest.java
@@ -18,8 +18,8 @@ public class MgbTest {
         //File configFile = new File("mbg_survey.xml");
         //File configFile = new File("mbg.xml");
         //File configFile = new File("mbg_fms.xml");
-        File configFile = new File("mbg_jiXiao.xml");
-        //File configFile = new File("mbg_joke.xml");
+        //File configFile = new File("mbg_jiXiao.xml");
+        File configFile = new File("mbg_joke.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(configFile);
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
